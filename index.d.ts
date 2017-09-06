2def422d9a56b013b78d837f428174444febc2c5
Maxim Syabro <maxim@syabro.com>
2017-09-06 13:15:19 +0300

    Added typings

---
+++ index.d.ts
@@ -0,0 +1,32 @@
+import * as React from 'react'
+
+type ComponentClass<P> = React.ComponentClass<P>;
+type StatelessComponent<P> = React.StatelessComponent<P>;
+
+
+interface ComponentDecorator<TOriginalProps, TOuterProps> {
+  (component: ComponentClass<TOriginalProps> | StatelessComponent<TOriginalProps>): ComponentClass<TOuterProps>;
+}
+
+interface MapStateToProps<TPropsFromState, TOuterProps> {
+  (state: any, ownProps?: TOuterProps): TPropsFromState;
+}
+
+
+export class TreeState {
+  public setState(newState: object): void
+}
+
+export class EnhancedComponent<ProxyProps = any, P = any, S = any> extends React.Component<P, S> {
+  stateProxy: ProxyProps
+
+  static connect(): void
+
+}
+
+export function getProvider<S, ProxyProps>(name: string, StateProxy?: new() => ProxyProps):  new() => EnhancedComponent<ProxyProps, any, S>
+
+export function connectTree<ProxyProps, TPropsFromState, TOuterProps>(
+  name: string,
+  mapStateToProps?: MapStateToProps<TPropsFromState, TOuterProps>,
+): ComponentDecorator<TPropsFromState & ProxyProps, TOuterProps>;
--- src/get-provider.js
+++ src/get-provider.js
@@ -1,12 +1,12 @@
 /* eslint react/prop-types: 0 */
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import {TreeState} from './tree-state'
+import { TreeState } from './tree-state'
+import { connectTree } from './connect-tree'
 
 export function getProvider(name, StateProxy = TreeState) {
   class Provider extends Component {
-    state = {}
-
+    stateProxy = null
     getState = () => this.state
     shouldComponentUpdate = () => false
 
@@ -14,6 +14,10 @@ export function getProvider(name, StateProxy = TreeState) {
       super(props, context)
       this.stateProxy = new StateProxy(this)
       this.state = props.state || {}
+    }
+
+    static connect(mapStateToProps) {
+      return connectTree(name, mapStateToProps)
     }
 
     getChildContext() {
