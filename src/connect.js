a348ab14431f81fc521b81fe7ff46b4c80d5353c
Maxim Syabro <maxim@syabro.com>
2018-05-26 13:39:53 +0300

    FEAT: Improve connect speed by setting state only when it's changed and storing data returned by stateToProps instead of the whole state

--- src/connect.js
+++ src/connect.js
@@ -1,12 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 
 export function connect(name, stateToProps) {
+  if (!stateToProps) {
+    throw Error(`stateToProps is undefined for ${name}`)
+  }
   return (InnerComponent) => {
-    class MapHoc extends Component {
+    class MapHoc extends React.Component {
+      mounted = false
       onStateChange = (newState) => {
-        this.setState(newState)
+        if (!this.mounted) return
+        let newLocalState = stateToProps(newState, this.props)
+        for (let key of Object.keys(newLocalState)) {
+          if (newLocalState[key] !== this.state[key]) {
+            this.setState(newLocalState)
+            break
+          }
+        }
       }
 
       constructor(props, context) {
@@ -19,13 +30,22 @@ export function connect(name, stateToProps) {
         }
         this.stateProxy = context[name]
 
-        this.state = this.stateProxy.getState()
+        this.state = stateToProps ? stateToProps(this.stateProxy.getState(), props) : {}
         this.stateProxy.onStateChange.add(this.onStateChange)
       }
 
+      componentDidMount() {
+        this.mounted = true
+      }
+
+      componentWillUnmount() {
+        this.mounted = false
+      }
+
+
       render() {
         const props = {
-          ...(stateToProps && stateToProps(this.state, this.props)),
+          ...this.state,
           [name]: this.stateProxy
         }
         return (
